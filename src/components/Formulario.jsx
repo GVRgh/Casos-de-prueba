3ea3cada715fe6fb4e6b8425549301d6dbd771c5
oelorduy@gmail.com <oelorduy@gmail.com>
2025-05-01 11:21:03 -0500

    Validacion de fomrulario campo individual

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -178,13 +178,13 @@ export const Formulario = ({
 
         <div className="mb-5">
           <label
-            htmlFor="alta"
+            htmlFor="cita"
             className="block text-gray-700 uppercase font-bold"
           >
-            Alta
+            Fecha de Cita
           </label>
           <input
-            id="alta"
+            id="cita"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={fecha}
