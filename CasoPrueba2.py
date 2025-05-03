d24c4ade0f059997ef0c418b6336528126e1e8a8
oelorduy@gmail.com <oelorduy@gmail.com>
2025-05-02 19:15:18 -0500

    comentarios caso prueba 2

--- CasoPrueba2.py
+++ CasoPrueba2.py
@@ -23,24 +23,25 @@ driver = webdriver.Chrome(
 )
 
 try:
+     # Abrir una url host:3000
     driver.get("http://localhost:3000/")
     time.sleep(2)
-
+     #Ingresa nombre de mascota
     driver.find_element(By.ID, "mascota").send_keys("macarena")
     time.sleep(2)
-
+    #Ingresa nombre de propietario
     driver.find_element(By.ID, "propietario").send_keys("Juan Perez")
     time.sleep(2)
-
+    #Ingresa e-mail del propietario
     driver.find_element(By.ID, "email").send_keys("juanperez@gmail.com")
     time.sleep(2)
-
+    #Ingresa fecha de la cita  
     driver.find_element(By.ID, "cita").send_keys("2025" + Keys.ARROW_RIGHT + "04-02")
     time.sleep(2)
-
+    #Ingresa observaciones del paciente
     driver.find_element(By.ID, "observaciones").send_keys("colitis de 4 dias de evolucion")
     time.sleep(2)
-
+    #Click al boton de agregar
     driver.find_element(By.ID, "agregar").click()
     time.sleep(3)
 
